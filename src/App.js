9dab88f449ed295eda609fbb139dc79fda64c4a4
Dillon Christensen <j.dillon.christensen@gmail.com>
2017-06-21 23:42:22 -0500

    a bit further and in react now

--- src/App.js
+++ src/App.js
@@ -10,20 +10,65 @@ const styles = {
     right: 0,
     top: 0,
     width: '100vw'
+};
+
+const searchStyles = {
+    form: {
+        bottom: '2em',
+        left: '2em',
+        position: 'fixed',
+        zIndex: 2
+    },
+    input: {
+        padding: '0.5em',
+        width: '250px'
+    }
 }
 
-class App extends Component {
+export default class App extends Component {
     map;
 
-  render() {
-    return (
-      <div style={styles} ref={elem => this.dom = elem}></div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    }
 
-  componentDidMount() {
-      this.map = new Maps(this.dom);
-  }
+    render() {
+        return (
+            <div>
+                <form style={searchStyles.form} onSubmit={this.search.bind(this)}>
+                    <input  style={searchStyles.input}
+                            placeholder="Search for places in bounds..."
+                            value={this.state.value}
+                            onChange={this.handleChange.bind(this)}/>
+                </form>
+                <div style={styles} ref={elem => this.dom = elem}></div>
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    search(e) {
+        e.preventDefault();
+        this.map.searchForPlace(this.state.value)
+            .then(results => {
+                results.map(r => this.map.addPlaceMarker(r.geometry.location, 'crimson'));
+            });
+    }
+
+    componentDidMount() {
+        this.map = new Maps(this.dom);
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(pos => {
+                this.map.setCenter({lat: pos.coords.latitude, lng: pos.coords.longitude});
+            }, () => {
+                console.error('CURRENT_LOCATION: Not permitted access to location');
+            });
+        } else {
+            console.error('CURRENT_LOCATION: Unsupported in current browser');
+        }
+    }
 }
-
-export default App;
--- src/maps.service.js
+++ src/maps.service.js
@@ -1,21 +1,77 @@
 export default class MapsService {
     _sdkPromise;
-    _map;
+    map;
+    places;
+    userMarker;
+    center;
 
     constructor(elem) {
-        this._map = this.sdk.then(() => {
-            return new window.google.maps.Map(elem, {
-                center: {lat: -34.397, lng: 150.644},
+        this.loadSDK().then(() => {
+            this.map = new window.google.maps.Map(elem, {
+                center: this.coords || {lat: -34.397, lng: 150.644},
                 zoom: 13
+            });
+            this.places = new window.google.maps.places.PlacesService(this.map);
+        });
+    }
+
+    setCenter(coords) {
+        this.center = coords;
+        if (this.map) {
+            this.map.setCenter(coords);
+            this.addUserMarker(coords);
+        }
+    }
+
+    addUserMarker(coords) {
+        if (this.userMarker) {
+            this.userMarker.setMap(null);
+        }
+        this.userMarker = new window.google.maps.Marker({
+            icon: {
+                path: window.google.maps.SymbolPath.CIRCLE,
+                fillColor: 'powderblue',
+                fillOpacity: 1,
+                strokeColor: 'dodgerblue',
+                strokeWeight: 2,
+                scale: 5
+            },
+            map: this.map,
+            position: coords
+        });
+    }
+
+    addPlaceMarker(coords, color) {
+        return new window.google.maps.Marker({
+            icon: {
+                path: window.google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+                fillColor: color,
+                fillOpacity: 0.7,
+                strokeColor: color,
+                strokeWeight: 2,
+                scale: 3
+            },
+            map: this.map,
+            position: coords
+        });
+    }
+
+    searchForPlace(keyword) {
+        return new Promise((resolve, reject) => {
+            this.places.nearbySearch({
+                bounds: this.map.getBounds(),
+                keyword: keyword
+            }, (results, status) => {
+                if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                    resolve(results);
+                } else {
+                    reject(status);
+                }
             });
         });
     }
 
-    get map() {
-        return this._map;
-    }
-
-    get sdk() {
+    loadSDK() {
         if (!this._sdkPromise) {
             this._sdkPromise = new Promise((resolve, reject) => {
                 const script = document.createElement('script');
